33505f90268fcfbd3887219e3e820b67c641f310
AbhiTaker <Abhi.taker20@gmail.com>
2017-01-27 14:23:43 +0530

    before git

---
+++ EM.js
@@ -0,0 +1,18 @@
+function getstuff(){
+	document.querySelector('#login').onclick=talk;
+	
+}
+function talk(){
+	window.alert('wrong password');
+}
+
+window.onload=getstuff;
+
+function getstuff(){
+	document.querySelector('#pic').onclick=talk;
+	
+}
+function talk(){
+	document.write('HE IS AWESOME');
+	
+}
