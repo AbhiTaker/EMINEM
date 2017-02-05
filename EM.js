d079a01c1f37cc34ed268fce84423ce5c7992e38
AbhiTaker <Abhi.taker20@gmail.com>
2017-02-05 14:13:54 +0530

    content added to discography pages

--- EM.js
+++ EM.js
@@ -1,3 +1,22 @@
+function dofirst(){
+	 var button = document.getElementById("login");
+	 button.addEventListener("onclick",store,false);
+}
+
+function store(){
+	var name = document.getElementById("name").value;
+	var pass = document.getElementById("pass").value;
+	sessionStorage.setitem(name,pass);
+	display(name);
+}
+
+function display(){
+	var main_section=document.getElementById("main_section");
+	var pass=sessionStorage.getitem(name);
+	main_section.innerHTML = "WELCOME" + name;
+}
+
+window.addEventListener("load",dofirst,false);
 function getstuff(){
 	document.querySelector('#login').onclick=talk;
 	
